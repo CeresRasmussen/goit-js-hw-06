e608f170408008a2fc696b3c259c4383cf7e29de
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-02-04 01:08:26 +0200

    task 1-3

---
+++ js/task-01.js
@@ -0,0 +1,16 @@
+// Напиши скрипт, який:
+
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+
+const listItem = document.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${listItem.length}`);
+const dmdm = listItem.forEach((el) => {
+  const category = el.querySelector("h2");
+  const categoriesElement = el.querySelectorAll("li");
+  console.log(`Category: ${category.textContent}`);
+  console.log(`Elements: ${categoriesElement.length}`);
+  //   console.log(`Category: ${el.querySelector("h2").textContent}`);
+  //   console.log(`Elements: ${el.querySelectorAll("li").length}`);
+});
---
+++ js/task-02.js
@@ -0,0 +1,17 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+const ingredientsUl = document.querySelector("#ingredients");
+// const ingredientsSet = ingredients.map((el) => `<li class="item">${el}</li>`).join("");
+
+const ingredientsSet = ingredients.reduce(
+  (prew, next) => prew + `<li class="item">${next}</li>`,
+  ""
+);
+ingredientsUl.innerHTML = ingredientsSet;
+// ingredientsUl.insertAdjacentHTML("afterbegin", ingredientsSet);
