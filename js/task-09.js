35c104595df12c0edf2d5e6476ea4b1ecab00d8b
CeresRasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-02-05 21:11:17 +0200

    task 8-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+// Завдання 9
+
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color
+// і виводить значення кольору в span.color.
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.body;
+const bodyColorName = document.querySelector(".color");
+const changeBtnColor = document.querySelector(".change-color");
+
+changeBtnColor.addEventListener("click", () => {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+  bodyColorName.textContent = `${getRandomHexColor()}`;
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,49 @@
+// Завдання 10
+
+// Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і
+// натискає кнопку Створити, після чого рендериться колекція.Натисненням на кнопку Очистити, колекція елементів очищається.
+// Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>,
+// скільки вказано в amount і додає їх у div#boxes.
+
+// Розміри найпершого <div> - 30px на 30px.
+// Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+// Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor
+// для отримання кольору.
+
+const quantityBoxInput = document.querySelector("input");
+const createBoxBtn = document.querySelector("[data-create]");
+const destroyBoxBtn = document.querySelector("[data-destroy]");
+const boxContainer = document.querySelector("#boxes");
+
+const boxes = [];
+let size = 30;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  for (let index = 0; index < amount; index++) {
+    size += 10;
+    const box = document.createElement("div");
+    box.style.backgroundColor = `${getRandomHexColor()}`;
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    boxes.push(box);
+  }
+  boxContainer.append(...boxes);
+}
+
+function quantityBox() {
+  const quantity = quantityBoxInput.value > 0 ? Number(quantityBoxInput.value) : 1;
+  createBoxes(quantity);
+}
+
+function cleanAll() {
+  boxContainer.innerHTML = "";
+  size = 30;
+  boxes.length = 0;
+}
+
+createBoxBtn.addEventListener("click", quantityBox);
+destroyBoxBtn.addEventListener("click", cleanAll);
